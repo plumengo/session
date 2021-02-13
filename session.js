1d9a0721eb9e85e5bb41409f9dc8b56c106b8441
plumengo <72603108+plumengo@users.noreply.github.com>
2021-02-13 17:49:05 +0100

    Update session.js

--- session.js
+++ session.js
@@ -1,5 +1,4 @@
 const uuid = require('uuid')
-//const cookie = require('cookie')
 const crypto = require('plumengo-crypto')
 const cache = require('node-cache')
 
@@ -13,7 +12,7 @@ function init(opts) {
     opts.maxAge = opts.maxAge || (3600 * 1000)
     opts.store = opts.store || 'node-cache'
 
-    switch(opts.store) {
+    switch (opts.store) {
         case 'node-cache':
             store = new cache()
             break;
@@ -30,31 +29,33 @@ function session(sessionClass, opts) {
     init(opts)
 
     return async function (ctx, next) {
-        // get key from cookies
+        let session, jsonString, jsonObject
+
+        // get encrypted value of opts.key from cookies 
         let encrypted = ctx.cookies.get(opts.key)
 
         if (encrypted == undefined) {
-            let session = new sessionClass()
-            // encrypt session object
-            encrypted = crypto.encrypt(JSON.stringify(session))
-            // store session in cookies
-            ctx.cookies.set(opts.key, encrypted, { httpOnly: false, expires: new Date(opts.maxAge + Date.now()) })
-            // store session in opts.store
-            store.set(encrypted, session, opts.maxAge)
-            // set ctx.session
-            ctx.session = JSON.parse(JSON.stringify(session))
-        } else {
-            // check session in opts.store
-            let session = store.get(encrypted)
+            session = new sessionClass()
+            jsonString = JSON.stringify(session)
+            jsonObject = JSON.parse(jsonString)
 
-            if (session == undefined) {
-                session = crypto.decrypt(encrypted)
-                // store session in opts.store
-                store.set(encrypted, session, opts.maxAge)
-            }
-            // set ctx.session
-            ctx.session = JSON.parse(JSON.stringify(session))
+            // encrypt JSON string os session object
+            encrypted = crypto.encrypt(jsonString)
+            // store encrypted value of jsonString in cookies in opts.key
+            ctx.cookies.set(opts.key, encrypted, { httpOnly: false, expires: new Date(opts.maxAge + Date.now()) })
+            
+        } else {
+            // check session in store or decrypt encrypted value
+            session = store.get(encrypted) || JSON.parse(crypto.decrypt(encrypted))
+            jsonString = JSON.stringify(session)
+            jsonObject = JSON.parse(jsonString)
         }
+
+        // store jsonObject in store
+        store.set(encrypted, jsonObject, opts.maxAge)
+        // set ctx.session to jsonObject
+        ctx.session = jsonObject
+
         // next middleware
         await next()
     };
